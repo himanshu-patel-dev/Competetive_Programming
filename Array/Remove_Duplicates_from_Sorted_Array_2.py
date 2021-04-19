7280346b500605e6d1481c3d0d149d6d778d0931
Himanshu Patel <py.himanshu.patel@gmail.com>
2021-04-19 16:05:10 +0530

    remove duplicate from array

--- Array/Remove_Duplicates_from_Sorted_Array_2.py
+++ Array/Remove_Duplicates_from_Sorted_Array_2.py
@@ -1,50 +1,33 @@
 '''
 Given a sorted array nums, remove the duplicates in-place such 
-that duplicates appeared at most twice and return the new length.
+that each element appears only once and returns the new length.
 
-Do not allocate extra space for another array; you must do this 
+Do not allocate extra space for another array, you must do this 
 by modifying the input array in-place with O(1) extra memory.
-
-Clarification:
-Confused why the returned value is an integer, but your answer 
-is an array?
-Note that the input array is passed in by reference, which means 
-a modification to the input array will be known to the caller.
-
-Internally you can think of this:
-// nums is passed in by reference. (i.e., without making a copy)
-int len = removeDuplicates(nums);
-
-// any modification to nums in your function would be known by the caller.
-// using the length returned by your function, it prints the first len elements.
-for (int i = 0; i < len; i++) {
-    print(nums[i]);
-}
-
-Constraints:
-
-0 <= nums.length <= 3 * 104
--104 <= nums[i] <= 104
-nums is sorted in ascending order.
 '''
 
+
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        count = 0
-        prev_char = 'x'
-        j = 0
+    def removeDuplicates(self, nums):
+        if len(nums) == 0:
+            return 0
+
+        first = 0
         
         for i in range(len(nums)):
-            nums[j] = nums[i]
-            
-            if nums[i] == prev_char and count > 1:
-                continue
-            
-            if prev_char == nums[i]:
-                count += 1
-            else:
-                prev_char = nums[i]
-                count = 1
-            j += 1
-            
-        return j
+            if nums[first] != nums[i]:
+                first += 1
+                nums[first] = nums[i]
+
+        return first+1
+
+if __name__ == "__main__":
+    s = Solution()
+
+    nums = [1,1,2]
+    print(s.removeDuplicates(nums))
+    print(nums)
+
+    nums = [0,0,1,1,1,2,2,3,3,4]
+    print(s.removeDuplicates(nums))
+    print(nums)
--- LC.py
+++ LC.py
@@ -1,66 +1,33 @@
 '''
-Constraints:
+Given a sorted array nums, remove the duplicates in-place such 
+that each element appears only once and returns the new length.
 
-1 <= s.length <= 30
-s consists of lowercase English letters, digits, and square brackets '[]'.
-s is guaranteed to be a valid input.
-All the integers in s are in the range [1, 300].
-
-Example 1:
-Input: s = "3[a]2[bc]"
-Output: "aaabcbc"
-
-Example 2:
-Input: s = "3[a2[c]]"
-Output: "accaccacc"
-
-Example 3:
-Input: s = "2[abc]3[cd]ef"
-Output: "abcabccdcdcdef"
-
-Example 4:
-Input: s = "abc3[cd]xyz"
-Output: "abccdcdcdxyz"
+Do not allocate extra space for another array, you must do this 
+by modifying the input array in-place with O(1) extra memory.
 '''
 
-'''
-Solution:
-T = O()
-'''
+
 class Solution:
-	def decodeString(self, s):
-		stack = []
+    def removeDuplicates(self, nums):
+        if len(nums) == 0:
+            return 0
 
-		for c in s:
-			# put each char in stack if its not ']'
-			if c != ']':
-				stack.append(c)
-				continue
+        first = 0
+        
+        for i in range(len(nums)):
+            if nums[first] != nums[i]:
+                first += 1
+                nums[first] = nums[i]
 
-			# if curr ele is end of a bracket then pop the 
-			# string in bw two [ and ] in MiddleString
-			MiddleString = []
-			while stack and stack[-1] != '[':
-				MiddleString.append(stack.pop())
-			MiddleString = ''.join(MiddleString[::-1])
-
-			stack.pop()	# pop out [
-
-			# pop out the number associated with MiddleString
-			num = []
-			while stack and stack[-1].isdigit():
-				num.append(stack.pop())
-			num = ''.join(num[::-1])
-
-			stack.extend( list(int(num)*MiddleString) )
-		
-		return ''.join(stack)
-
+        return first+1
 
 if __name__ == "__main__":
-	s = Solution()
+    s = Solution()
 
-	print( s.decodeString('3[a]2[bc]') )
-	print( s.decodeString('3[a2[c]]') )
-	print( s.decodeString('2[abc]3[cd]ef') )
-	print( s.decodeString('abc3[cd]xyz') )
+    nums = [1,1,2]
+    print(s.removeDuplicates(nums))
+    print(nums)
+
+    nums = [0,0,1,1,1,2,2,3,3,4]
+    print(s.removeDuplicates(nums))
+    print(nums)
